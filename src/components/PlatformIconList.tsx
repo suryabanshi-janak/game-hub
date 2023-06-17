f6ba5c492da6a15a18fc91650197ad8c58433811
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-06-17 17:58:08 +0545

    Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,42 @@
+import { HStack, Icon } from '@chakra-ui/react';
+import { IconType } from 'react-icons';
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from 'react-icons/fa';
+import { SiNintendo } from 'react-icons/si';
+import { MdPhoneIphone } from 'react-icons/md';
+import { BsGlobe } from 'react-icons/bs';
+import { Platform } from '../hooks/useGames';
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color='gray.500' />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
