75a5abd1f7a1d8b5899752c6e064f077e4e0f01d
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-06-17 23:17:01 +0545

    Create genre list

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,9 +13,9 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding='10px'
-        spacing={10}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding={10}
+        spacing={4}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -17,7 +17,7 @@ const useData = <T>(endpoint: string) => {
 
     setIsLoading(true);
     apiClient
-      .get<FetchResponse<T>>(endpoint)
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
       .then((response) => {
         setData(response.data.results);
         setIsLoading(false);
