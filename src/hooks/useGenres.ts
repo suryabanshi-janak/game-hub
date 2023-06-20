c45fa62a2b6578ed87557945773bfd485b57367c
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-06-20 23:25:52 +0545

    Refactor - simplify time calculation

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
+
 import APIClient from '../services/api-client';
 import genres from '../data/genres';
 
@@ -14,7 +16,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24hrs
+    staleTime: ms('24h'),
     initialData: genres,
   });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
+
 import platforms from '../data/platforms';
 import APIClient from '../services/api-client';
 
@@ -14,7 +16,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms('24h'),
     initialData: platforms,
   });
 
